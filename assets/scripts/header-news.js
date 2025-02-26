3b9e5bd9d2ccbfcf1f4382240d1bbc8ccaffa6d4
Petunia <psofia20056@gmail.com>
2025-02-26 20:30:43 +0000

    atualizei a página de notícias

--- assets/scripts/header-news.js
+++ assets/scripts/header-news.js
@@ -1,6 +1,5 @@
 window.addEventListener('scroll', () => {
   const header = document.querySelector('.header');
-  header.style.display = `flex`;
   const newsImage = document.querySelector('.news-image');
 
   const scrollPosition = window.scrollY;
