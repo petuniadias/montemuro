56c5ce825837fa1778ecc72a6a1a8bd51a8fdd7c
Petunia <psofia20056@gmail.com>
2025-02-27 20:40:34 +0000

    atualização do footer e criação da página onde estãp todas as notícias.

---
+++ assets/scripts/all-news.js
@@ -0,0 +1,18 @@
+function displayDescription() {
+  document.querySelectorAll('.news-box').forEach(box => {
+    const newsDescription = box.querySelector('.news-description');
+    const newsTitle = box.querySelector('.news-title');
+
+    box.addEventListener('mouseenter', () => {
+      newsDescription.style.display = 'block';
+      newsTitle.style.bottom = `60px`;
+    });
+    box.addEventListener('mouseleave', () => {
+      newsDescription.style.display = 'none';
+      newsTitle.style.bottom = '0px';
+      
+    });
+  });
+}
+
+displayDescription();
