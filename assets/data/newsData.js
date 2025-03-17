1732de3cf57a280581f1e461e13a41c1fab119e0
Petunia <psofia20056@gmail.com>
2025-03-17 14:23:09 +0000

    Adicionei função de renderização das notícias

---
+++ assets/data/newsData.js
@@ -0,0 +1,19 @@
+export const newsData = [
+  {
+    title: "Ovelha Xoné ataca cidade e instaura o pânico",
+    description: "Hoje, na cidade de Petersburgo, ovelha Xoné não deixou mágoas para trás",
+    image: "assets/images/ovelha xone.jpg"
+  }, {
+    title: "Galinhas presas na Turquia",
+    description: "Hoje, na cidade de Petersburgo, ovelha Xoné não deixou mágoas para trás",
+    image: "assets/images/ovelhas.jpg"
+  }, {
+    title: "Galinhas presas na Turquia",
+    description: "Hoje, na cidade de Petersburgo, ovelha Xoné não deixou mágoas para trás",
+    image: "assets/images/ovelhas no campo.jpg"
+  }, {
+    title: "Carneiros assaltam banco",
+    description: "Hoje, na cidade de Petersburgo, ovelha Xoné não deixou mágoas para trás",
+    image: "assets/images/boi e carneiro.jpg"
+  }
+];
--- assets/scripts/all-news.js
+++ assets/scripts/all-news.js
@@ -1,3 +1,27 @@
+import {newsData} from '../data/newsData.js';
+
+function renderNews() {
+  const newsContainer = document.querySelector('.news');
+  let newsSummaryHTML = '';
+
+  newsData.forEach(item => {
+    newsSummaryHTML += `
+    <a class="news-box" href="news.html">
+      <img class="news-img" src="${item.image}">
+      <div class="news-title-box"></div>
+      <h2 class="news-title">${item.title}</h2>
+      <p class="news-description">${item.description}</p>
+    </a>
+    `;
+  });
+  
+  newsContainer.innerHTML = newsSummaryHTML;
+  
+  displayDescription();
+}
+
+renderNews();
+
 function displayDescription() {
   document.querySelectorAll('.news-box').forEach(box => {
     const newsDescription = box.querySelector('.news-description');
@@ -14,5 +38,3 @@ function displayDescription() {
     });
   });
 }
-
-displayDescription();
